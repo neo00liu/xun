79d26ec797440a3672e09d1c54f0afd5ff36a8a9
neo00liu <neo.liu0000@gmail.com>
2013-12-18 15:57:40 +0800

    first commit

---
+++ Xun/Xun/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Xun
+//
+//  Created by apple on 13-12-18.
+//  Copyright (c) 2013年 Neo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Xun/Xun/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Xun
+//
+//  Created by apple on 13-12-18.
+//  Copyright (c) 2013年 Neo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
